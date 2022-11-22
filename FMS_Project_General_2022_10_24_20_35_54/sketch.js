711412094750bda7abe4458b743a0160d7fbc0cd
bikonoai <47820685+bikonoai@users.noreply.github.com>
2022-11-22 16:13:39 -0700

    Add files via upload

--- FMS_Project_General_2022_10_24_20_35_54/sketch.js
+++ FMS_Project_General_2022_10_24_20_35_54/sketch.js
@@ -1,14 +1,23 @@
+
 //Setup is only executed once immediately after running
-function setup() {
- createCanvas(400, 500);
+let theta;
+
+function setup() { 
+  createCanvas(400, 500);
+  theta = PI * 2 / 5;
 }
 
 //draw is executed infinitely while the program runs
 let choice = 0;
 let x = 90;
+let starX=0;
+let starY=0;
+let p1;
+let p2;
+let counterStar;
+let bestScore = 0;
+
 function draw() {
-  clear()
-  print(choice)
  switch(choice) {
    case 0:
      renderMainMenu();
@@ -22,16 +31,21 @@ function draw() {
    case 3:
      renderGame3();
      break; 
+   case 4:
+     pauseGame();
   }
 }
 
 function renderMainMenu() {
-  stroke('black');
+  stroke('white');
+  fill('black');
   clear();
   background('white');
   textSize(22);
   fill(40);
   text('Hello, User', 40, 50, 200, 200);
+  
+  counterStar = 0;
   
   let img1;
   function preload() {
@@ -39,11 +53,16 @@ function renderMainMenu() {
   }
   
   let col = color('#6699ff');
+  
   btn1 = createButton('Exercise 1');
   btn1.position(40, 120);
   btn1.style('background-color', col);
   btn1.mousePressed(() => {choice = 1;
+                           clear();
                            removeElements();
+                           //myStar(this.starX, this.starY, this.outRad, this.inRad);
+                            p1 = random(width);
+                            p2 = random(height);
                           }
                    );
   
@@ -68,11 +87,66 @@ function renderMainMenu() {
   text('Enjoy exercising!', 40, 390, 200, 200);
 }
 
+function mousePressed() {
+  switch(choice) {
+    case 1:
+       //myStar(starX, starY, 50, 25).myClick();
+        myClick();
+  }
+}
+
+let star;
 function renderGame1() {
- //console.log('here');
- background(220);
- square(x, 220, 55);
- x = x+1;
+  strokeWeight(4);
+  stroke(0);
+  fill(255, 204, 0);
+  text("Tap on a star", 150, 100);
+  
+  let col = color('#6699ff');
+  btnPause = createButton("Pause");
+  btnPause.position(320, 30);
+  btnPause.style('background-color', col); 
+  btnPause.mousePressed(() => {
+                                choice = 4;
+                                removeElements();
+                              }
+                       );
+  
+ 
+  star = new myStar(p1, p2, 50, 25);
+}
+
+function pauseGame () {
+  clear();
+  background(220);
+  // textSize(20);
+  // fill(40);
+  text("You paused the game", 100, 70);
+  
+  let col = color('#6699ff');
+  btnResume = createButton("Resume the game");
+  btnResume.position(50, 150);
+  btnResume.style('background-color', col); 
+  btnResume.mousePressed(() => {
+                                choice = 1;
+                                removeElements();
+                                clear();
+                              }
+                       );
+  
+  
+  btnMenu = createButton("Go back to main menu");
+  btnMenu.position(50, 250);
+  btnMenu.style('background-color', col); 
+  btnMenu.mousePressed(() => {
+                                choice = 0;
+                                removeElements();
+                                clear();
+                              }
+                       );
+  
+  //text("Your best score is " + bestScore, 50, 300);
+  
 }
 
 function renderGame2 () {
@@ -89,4 +163,42 @@ function renderGame3(){
   textSize(20);
  fill(40);
  text('The exercise 3 is succesfully laucnhed', 40, 390, 200, 200);
+}
+
+//STAR DEFINITION BELOW
+
+function myStar(starX, starY, outRad, inRad) {
+  this.starX = random(width);
+  this.starY = random(height);
+  this.outRad = 50;
+  this.inRad = 25;
+  beginShape();
+  
+  for (let i = 0; i < 5; i++) {
+   vertex(starX + cos(i * theta) * outRad, starY + sin(i * theta) * outRad);
+   vertex(starX + cos((i + 0.5) * theta) * inRad, starY + sin((i + 0.5) * theta) * inRad);
+  }
+  endShape(CLOSE);
+} 
+
+this.display = function() {
+  strokeWeight(4);
+  stroke(0);
+}
+
+function myClick() {
+  var d = dist(mouseX, mouseY, p1, p2);
+  if (d < 25) {
+    clear();
+    p1 = random(width);
+    p2 = random(height);
+    myStar(p1, p2, 50, 25);
+    counterStar += 1;
+  }
+  
+  // if (counter > bestScore) {
+  //   bestScore = counter;
+  // }
+  
+  text("Your score: " + counterStar, 10, 50);
 }
